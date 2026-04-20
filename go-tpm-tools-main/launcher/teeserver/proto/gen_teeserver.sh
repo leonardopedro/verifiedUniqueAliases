27ccf71e1975a84f2a83d1c69c4726c1e5bcca0e
leonardopedro <leonardopedro4@gmail.com>
2026-04-20 17:41:12 +0200

    v116-PROD-FIX

--- go-tpm-tools-main/launcher/teeserver/proto/gen_teeserver.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-protoc -I. -I../../../ -I../../../proto --go_out=. --go_opt=module=github.com/google/go-tpm-tools/launcher/teeserver/proto  --experimental_allow_proto3_optional teeserver.proto
